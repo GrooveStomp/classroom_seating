ee52fcaf65372decebb4b3eb29c12abf094347b2
Aaron Oman <groovestomp@groovestomp.com>
2020-06-18 05:34:18 -0700

    Adds svelte-routing and integrates it into the UI

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -3,10 +3,22 @@
   import Logout from './logout.svelte'
 
   import { user } from './stores.js';
+  import { Router, Route, Link } from "svelte-routing";
+  import Home from "./routes/home.svelte";
+
+  export let url = "";
 </script>
 
-{#if !$user.authToken}
-  <Login/>
-{:else}
-  <Logout/>
-{/if}
+<Router url="{url}">
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="login">Login</Link>
+    <Link to="logout">Logout</Link>
+  </nav>
+  <div>
+    <Route path="/" component="{Home}" />
+    <Route path="login" component="{Login}" />
+    <Route path="logout" component="{Logout}" />
+    <!-- <Route path="blog/:id" component="{BlogPost}" /> -->
+  </div>
+</Router>
